293bb59d6f70a194f5a96428a6f8690c8f923f8c
rafi10hasan <rafihasan0075@gmail.com>
2025-12-22 11:21:45 +0600

    working with cloudinary

---
+++ src/app/config/cloudinary.config.ts
@@ -0,0 +1,16 @@
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;
+
+// V0-FhDUFsgyxcnBCEEIgvrBJ8S4
+// 414126649738475
+//  dqk9g25o1
--- src/app/lib/upload.ts
+++ src/app/lib/upload.ts
@@ -1,50 +1,67 @@
 import multer from 'multer';
-import path from 'path';
-// import { v4 as uuidv4 } from 'uuid';
-import { AppError } from '../utils';
-import httpStatus from 'http-status';
-import fs from 'fs';
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    let folderPath = './public';
+const storage = multer.memoryStorage();
 
-    if (file.mimetype.startsWith('image')) {
-      folderPath = './public/images';
-    } else if (file.mimetype.startsWith('video')) {
-      folderPath = './public/videos';
-    } else {
-      callback(
-        new AppError(
-          httpStatus.BAD_REQUEST,
-          'Only images and videos are allowed'
-        ),
-        './public'
-      );
-      return;
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+  fileFilter: (_req, file, cb) => {
+    if (!file.mimetype.startsWith('image')) {
+      cb(new Error('Only image files are allowed'));
     }
-
-    // Check if the folder exists, if not, create it
-    if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath, { recursive: true });
-    }
-
-    callback(null, folderPath);
-  },
-
-  filename(_req, file, callback) {
-    const fileExt = path.extname(file.originalname);
-    const fileName = `${file.originalname
-      .replace(fileExt, '')
-      .toLocaleLowerCase()
-      .split(' ')
-      .join('-')}-${Date.now()}`;
-    // .join('-')}-${uuidv4()}`;
-
-    callback(null, fileName + fileExt);
+    cb(null, true);
   },
 });
 
-const upload = multer({ storage });
-
 export default upload;
+
+// import multer from 'multer';
+// import path from 'path';
+// // import { v4 as uuidv4 } from 'uuid';
+// import { AppError } from '../utils';
+// import httpStatus from 'http-status';
+// import fs from 'fs';
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     let folderPath = './public';
+
+//     if (file.mimetype.startsWith('image')) {
+//       folderPath = './public/images';
+//     } else if (file.mimetype.startsWith('video')) {
+//       folderPath = './public/videos';
+//     } else {
+//       callback(
+//         new AppError(
+//           httpStatus.BAD_REQUEST,
+//           'Only images and videos are allowed'
+//         ),
+//         './public'
+//       );
+//       return;
+//     }
+
+//     // Check if the folder exists, if not, create it
+//     if (!fs.existsSync(folderPath)) {
+//       fs.mkdirSync(folderPath, { recursive: true });
+//     }
+
+//     callback(null, folderPath);
+//   },
+
+//   filename(_req, file, callback) {
+//     const fileExt = path.extname(file.originalname);
+//     const fileName = `${file.originalname
+//       .replace(fileExt, '')
+//       .toLocaleLowerCase()
+//       .split(' ')
+//       .join('-')}-${Date.now()}`;
+//     // .join('-')}-${uuidv4()}`;
+
+//     callback(null, fileName + fileExt);
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// export default upload;
